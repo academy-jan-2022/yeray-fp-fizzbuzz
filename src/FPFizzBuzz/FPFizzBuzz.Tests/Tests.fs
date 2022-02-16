5949edef4391f7f4673e34b4d43ffa604a0c6ba1
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-16 15:11:44 +0100

    indent

--- src/FPFizzBuzz/FPFizzBuzz.Tests/Tests.fs
+++ src/FPFizzBuzz/FPFizzBuzz.Tests/Tests.fs
@@ -6,5 +6,5 @@ open Xunit
 
 [<Fact>]
 let ``convert number 1 to string`` () =
-    let result = FizzBuzz.convert 1
-    Assert.Equal("1", result)
+  let result = FizzBuzz.convert 1
+  Assert.Equal("1", result)
